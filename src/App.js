d78edff465b6f3aad083c2bec7b7a4d1b5e5b78e
Aishwarya Joy <aishwarya.joy@vonnue.com>
2021-12-07 18:34:14 +0530

    modified the App.js file and deleted unused files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { getDefaultNormalizer } from '@testing-library/dom';
 
 function App() {
+  const title = "Welcome Aishwarya"
+  const likes = 50;
+  const link = "http://www.google.com"
+  // const person ={
+  //   name : 'aishwarya',
+  //   mail : 'xxxx@gmail.com'
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <a href={link}>Google Site</a>
+        
+      </div>
     </div>
   );
 }
